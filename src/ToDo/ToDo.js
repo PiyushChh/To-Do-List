c9e5835bbe19c96b7da6aff3e5de594d1d75bdfa
Piyush Chhawachharia <pichhawachharia891@gmail.com>
2024-05-09 19:36:04 +0530

    Checkpoint 3 reached

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -1,9 +1,13 @@
+import Item from "../Item/Item";
 export default function ToDo(){
+    const todos= ["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"];
     return(
         <>
         <div>
         <h1>TO Do App</h1>
-        <h2>Nothing to do buddy. Sleep!!</h2>
+        {todos.map((item,index)=>(
+            <Item key={index} itemName={item}/>
+        ))}
         </div>
         </>
     )
